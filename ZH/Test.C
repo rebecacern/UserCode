5e20f3331b3bd06ad6a63a7bde4836d62181e9a2
rebeca <>
2012-11-14 16:34:22 +0000

    fist ZH

---
+++ ZH/Test.C
@@ -0,0 +1,158 @@
+// rebeca@cern.ch
+// First attempt to ZH code
+// November 2012
+
+#include "../Smurf/Core/SmurfTree.h"
+#include "../Smurf/Analysis/HWWlvlv/factors.h"
+#include "../Smurf/Core/LeptonScaleLookup.h"
+#include <TROOT.h>
+#include <TFile.h>
+#include <TTree.h>
+#include <TChain.h>
+#include <iostream>
+#include <fstream>
+#include "TLegend.h"
+#include "TPaveText.h"
+#include "TRandom.h"
+#include "TH1D.h"
+#include "TH2D.h"
+#include "TMath.h"
+#include "TCanvas.h"
+#include "TSystem.h"
+
+const int verboseLevel =   1;
+const double mz = 91.1876;
+
+void Test() {
+  
+  TString bgdInputFile    = "samples/backgroundA_3l.root";
+  TString dataInputFile   = "samples/data_3l.root";
+  TString sigInputFile   =  "samples/hww120.root";
+  
+  SmurfTree background;
+  background.LoadTree(bgdInputFile,-1);
+  background.InitTree(0);
+  
+  SmurfTree data;
+  data.LoadTree(dataInputFile,-1);
+  data.InitTree(0);
+  
+  SmurfTree signal;
+  signal.LoadTree(dataInputFile,-1);
+  signal.InitTree(0);
+  
+  char output[200];
+  sprintf(output,"histo_test.root");     
+  TFile* outFileNjets = new TFile(output,"recreate");
+  
+  TH1F* types = new TH1F("types", "types", 80, -0.5 , 79.5);
+  TH1D* bckg_met = new TH1D("bckg_met", "MET", 200, 0, 200);
+  TH1D* bckg_mllz = new TH1D("bckg_mllz", "m_{ll}", 200, 0, 200);
+  TH1D* bckg_mt = new TH1D("bckg_mt", "m_t", 200, 0, 200);
+  TH1D* bckg_ptjet = new TH1D("bckg_ptjet", "P_t of leading jet", 200, 0, 200);
+  
+  TH1D* sig_met = new TH1D("sig_met", "MET", 200, 0, 200);
+  TH1D* sig_mllz = new TH1D("sig_mllz", "m_{ll}", 200, 0, 200);
+  TH1D* sig_mt = new TH1D("sig_mt", "m_t", 200, 0, 200);
+  TH1D* sig_ptjet = new TH1D("sig_ptjet", "P_t of leading jet", 200, 0, 200);
+  
+  int nBgd=background.tree_->GetEntries();
+  for (int i=0; i<nBgd; ++i) {
+    
+    if (i%100000 == 0 && verboseLevel > 0)
+      printf("--- reading event %5d of %5d\n",i,nBgd);
+    background.tree_->GetEntry(i);
+    
+    if (background.njets_ != 2) continue;
+    if (background.lid3_ == background.lid2_ && background.lid3_ == background.lid1_) continue;
+    if (background.lid3_ == background.lid2_ && fabs(background.lid3_) != fabs(background.lid1_)) continue;
+    if (background.lid3_ == background.lid1_ && fabs(background.lid3_) != fabs(background.lid2_)) continue;
+    if (background.lid2_ == background.lid1_ && fabs(background.lid2_) != fabs(background.lid3_)) continue;
+    
+    double m[3] = {0, 0, 0};
+    LorentzVector pair1, pair2, pair3;
+    if (fabs(background.lid1_) == fabs(background.lid2_) && background.lq1_*background.lq2_ < 0){
+     pair1 = background.lep1_ + background.lep2_ ;
+     m[0] = pair1.M();
+    }
+    if (fabs(background.lid2_) == fabs(background.lid3_) && background.lq2_*background.lq3_ < 0){
+      pair2 = background.lep2_ + background.lep3_ ;
+      m[1] = pair2.M();
+    }
+    if (fabs(background.lid1_) == fabs(background.lid3_) && background.lq1_*background.lq3_ < 0){
+      pair3 = background.lep1_ + background.lep3_ ;
+      m[2] = pair3.M();
+    }
+    
+    if ( (m[0] < 80 || m[0] > 100) &&  (m[1] < 80 || m[1] > 100) &&  (m[2] < 80 || m[2] > 100)) continue;
+    
+   double min = TMath::Min(TMath::Min(fabs(mz -m[0]), fabs(mz-m[1])), TMath::Min(fabs(mz -m[0]), fabs(mz-m[2])));
+   
+   LorentzVector pair;
+   double mt;
+   if (min == fabs(mz - m[0])){  pair = pair1; mt =  background.mt3_;} 
+   else if (min == fabs(mz - m[1])){  pair = pair2;  mt =  background.mt1_;} 
+   else if (min == fabs(mz - m[2])){  pair = pair3;  mt =  background.mt2_;} 
+   
+   if (mt < 40 || background.met_ < 25) continue;
+   types->Fill(background.dstype_);
+   bckg_met->Fill(background.met_);
+   bckg_mllz->Fill(pair.M());
+   bckg_mt->Fill(mt);
+   bckg_ptjet->Fill(background.jet1_.Pt());
+   
+  }
+  
+  int nSig=signal.tree_->GetEntries();
+  for (int i=0; i<nSig; ++i) {
+    
+    if (i%100000 == 0 && verboseLevel > 0)
+      printf("--- reading event %5d of %5d\n",i,nBgd);
+    signal.tree_->GetEntry(i);
+    
+    if (signal.njets_ != 2) continue;
+    if (signal.lid3_ == signal.lid2_ && signal.lid3_ == signal.lid1_) continue;
+    if (signal.lid3_ == signal.lid2_ && fabs(signal.lid3_) != fabs(signal.lid1_)) continue;
+    if (signal.lid3_ == signal.lid1_ && fabs(signal.lid3_) != fabs(signal.lid2_)) continue;
+    if (signal.lid2_ == signal.lid1_ && fabs(signal.lid2_) != fabs(signal.lid3_)) continue;
+    
+    double m[3] = {0, 0, 0};
+    LorentzVector pair1, pair2, pair3;
+    if (fabs(signal.lid1_) == fabs(signal.lid2_) && signal.lq1_*signal.lq2_ < 0){
+      pair1 = signal.lep1_ + signal.lep2_ ;
+      m[0] = pair1.M();
+    }
+    if (fabs(signal.lid2_) == fabs(signal.lid3_) && signal.lq2_*signal.lq3_ < 0){
+      pair2 = signal.lep2_ + signal.lep3_ ;
+     m[1] = pair2.M();
+    }
+    if (fabs(signal.lid1_) == fabs(signal.lid3_) && signal.lq1_*signal.lq3_ < 0){
+      pair3 = signal.lep1_ + signal.lep3_ ;
+      m[2] = pair3.M();
+    }
+    
+    if ( (m[0] < 80 || m[0] > 100) &&  (m[1] < 80 || m[1] > 100) &&  (m[2] < 80 || m[2] > 100)) continue;
+    
+    double min = TMath::Min(TMath::Min(fabs(mz -m[0]), fabs(mz-m[1])), TMath::Min(fabs(mz -m[0]), fabs(mz-m[2])));
+    
+    LorentzVector pair;
+    double mt;
+    if (min == fabs(mz - m[0])){  pair = pair1; mt =  signal.mt3_;} 
+    else if (min == fabs(mz - m[1])){  pair = pair2;  mt =  signal.mt1_;} 
+    else if (min == fabs(mz - m[2])){  pair = pair3;  mt =  signal.mt2_;} 
+    
+    if (mt < 40 || signal.met_ < 25) continue;
+    types->Fill(signal.dstype_);
+    sig_met->Fill(signal.met_);
+    sig_mllz->Fill(pair.M());
+    sig_mt->Fill(mt);
+    sig_ptjet->Fill(signal.jet1_.Pt());  
+    
+  }
+  
+  
+  outFileNjets->Write();
+  outFileNjets->Close();
+  
+  
+}
---
+++ ZH/plot.C
@@ -0,0 +1,65 @@
+// rebeca@cern.ch
+// Basic plotter - comparing quantities
+// November 2012
+
+void plot(){
+  
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(0);
+  gStyle->SetErrorX(0);
+  
+  gStyle->SetOptStat(0);
+  gStyle->SetPalette(1);
+  
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetCanvasColor(kWhite);
+  gStyle->SetCanvasDefH(600);
+  gStyle->SetCanvasDefW(600);
+  gStyle->SetLabelFont(18,"");
+  
+  char myRootFile[300];
+  sprintf(myRootFile,"histo_test.root");
+  
+  TFile *_file0 = TFile::Open(myRootFile);
+  
+  const int nPlots = 4;
+  TString cutLabel[nPlots] =     { "met", "mllz", "mt", "ptjet"};
+  int rebinHisto[nPlots] =       { 4, 4, 4, 4};
+  TString cutTitle[nPlots] =     { "Missing E_{T}", "Invariant Mass", "Transverse mass (W)", "P_{T} of the leading jet"};
+  TH1F*  h [2][nPlots];
+  
+  for (const int iPlot = 0; iPlot < nPlots; iPlot++){
+    h[0][iPlot] = (TH1F*) _file0->Get("sig_" + cutLabel[iPlot]);
+    h[1][iPlot] = (TH1F*) _file0->Get("bckg_" + cutLabel[iPlot]);
+    
+    h[0][iPlot]->SetLineColor(kRed);
+    h[0][iPlot]->SetLineWidth(2);
+    h[0][iPlot]->SetNormFactor(1);
+    h[0][iPlot]->Rebin(4);
+    
+    h[1][iPlot]->SetLineColor(kBlue);
+    h[1][iPlot]->SetLineWidth(2);
+    h[1][iPlot]->SetNormFactor(1);
+    h[1][iPlot]->Rebin(4);
+    
+    leg = new TLegend(0.7,0.7,0.94,0.94);
+    leg ->SetFillStyle(1001);
+    leg ->SetFillColor(kWhite);
+    leg ->SetBorderSize(1);
+    leg->AddEntry(h[0][iPlot], "m_{H} = 120", "l");
+    leg->AddEntry(h[1][iPlot], "Background", "l");
+    
+    TCanvas *c1 = new TCanvas();
+    h[0][iPlot]->Draw();
+    h[1][iPlot]->Draw("sames");
+    sig_ptjet->GetYaxis()->SetTitle("Normalized to 1");
+    sig_ptjet->GetXaxis()->SetTitle(cutTitle[iPlot]);
+    sig_ptjet->GetYaxis()->CenterTitle(); 
+    leg->Draw();
+    
+    c1->SaveAs("plots/"+cutLabel[iPlot]+".png");
+    c1->SetLogy();
+    c1->SaveAs("plots/"+cutLabel[iPlot]+"_log.png");
+  }
+  
+}
