7942e497f39e14697d5f4d2eb3cec82fe1ee8997
rebeca <>
2013-02-07 14:25:32 +0000

    script to run

---
+++ ZH/run.sh
@@ -0,0 +1,7 @@
+rm 125/*
+root.exe -b -l -q chain.C+\(0\,125\)
+root.exe -b -l -q chain.C+\(1\,125\)
+root.exe -b -l -q chain.C+\(2\,125\)
+root.exe -b -l -q chain.C+\(3\,125\)
+root.exe -b -l -q chain.C+\(4\,125\)
+root.exe -b -l -q chain.C+\(5\,125\)
